283ca23fdb8a0b7e14884ac17656c3c9dcaa0072
devmoath <moath.alhajrii@gmail.com>
2022-11-13 13:57:46 +0300

    wip
    Signed-off-by: devmoath <moath.alhajrii@gmail.com>

--- rector.php
+++ rector.php
@@ -2,23 +2,35 @@
 
 declare(strict_types=1);
 
+use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
+use Rector\CodingStyle\Rector\Encapsed\EncapsedStringsToSprintfRector;
+use Rector\CodingStyle\Rector\Encapsed\WrapEncapsedVariableInCurlyBracesRector;
 use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\Set\ValueObject\SetList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
         __DIR__.'/src',
     ]);
 
-//    $rectorConfig->rules([
-//        InlineConstructorDefaultToPropertyRector::class,
-//    ]);
-//
-//    $rectorConfig->sets([
-//        LevelSetList::UP_TO_PHP_81,
-//        SetList::CODE_QUALITY,
-//        SetList::DEAD_CODE,
-//        SetList::EARLY_RETURN,
-//        SetList::TYPE_DECLARATION,
-//        SetList::PRIVATIZATION,
-//    ]);
+    $rectorConfig->rules([
+        InlineConstructorDefaultToPropertyRector::class,
+    ]);
+
+    $rectorConfig->sets([
+        LevelSetList::UP_TO_PHP_81,
+        SetList::CODE_QUALITY,
+        SetList::DEAD_CODE,
+        SetList::EARLY_RETURN,
+        SetList::TYPE_DECLARATION,
+        SetList::PRIVATIZATION,
+        SetList::CODING_STYLE,
+        SetList::NAMING,
+    ]);
+
+    $rectorConfig->skip([
+        EncapsedStringsToSprintfRector::class,
+        WrapEncapsedVariableInCurlyBracesRector::class,
+    ]);
 };
